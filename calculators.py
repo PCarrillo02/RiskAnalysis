9dca0944bf824ec5048d0e091dbbc82df4c925a6
PCarrillo02 <56772825+PCarrillo02@users.noreply.github.com>
2021-01-26 13:37:09 -0600

    Update calculators.py

--- calculators.py
+++ calculators.py
@@ -180,7 +180,7 @@ def calc_v2():
     air_ex = list(db.execute("SELECT air_exchange FROM calculations"))
 
     # Actual Calculations
-    Dmask = 1 - (float(filtration_mask[0]["filtration_eff"])
+    Dmask = 1 - (float(filtration_mask[0]["filtration_eff"]))
     volume = (length_cm * width_cm * height_cm) / 1000
 
     infected = float(students[0]["students"]) * float(positivity[0]["positivity"])/100
